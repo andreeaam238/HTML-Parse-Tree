c9afd178cac8e33e1e217cb19c7c8a14611ed835
andreeaam238 <57097532+andreeaam238@users.noreply.github.com>
2020-10-30 17:47:06 +0200

    Add files via upload

---
+++ structures.h
@@ -0,0 +1,21 @@
+/* Blaga Ana-Maria-Andreea - 314CB */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef _STRUCTURES_
+#define _STRUCTURES_
+
+/* Lista generica */
+
+typedef struct cell {
+  void *info;
+  struct cell *urm;
+} TCell, *ACell;
+
+void DistrLG(ACell *list);
+int InsLG(ACell *List, void *elem);
+
+#endif /* _STRUCTURES_ */
